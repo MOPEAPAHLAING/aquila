86480a736859c02efbeef5c362a735387099b467
MOEPAPAHLAING <moepapahlaing21@gmail.com>
2024-06-19 17:17:57 +0630

    Add wp_head() and wp_footer()

--- index.php
+++ index.php
@@ -4,15 +4,11 @@
 *
 *@package aquila
 */
+
+get_header();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Wordpress Theme</title>
-</head>
-<body>
-	hello
-</body>
-</html>
+
+	<div class="content">Content</div>
+	
+<?php
+get_footer();
