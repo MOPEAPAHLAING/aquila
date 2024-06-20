bae87055967660d2127307b3268e662a5f7e95a2
MOEPAPAHLAING <moepapahlaing21@gmail.com>
2024-06-20 10:06:01 +0630

    add bootstrap style and script

--- functions.php
+++ functions.php
@@ -5,9 +5,15 @@
 *@package Aquila
 */
 
+
 function aquila_theme_scripts () {
+	$url  = get_template_directory_uri();
+
 	wp_enqueue_style('style-css', get_stylesheet_uri());
-	wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/main.js', [], true);
+	wp_enqueue_style('bootstrap-css', $url . '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
+
+	wp_enqueue_script('main-js', $url . '/assets/main.js', [], true);
+	wp_enqueue_script('bootstrap-js', $url . 'assets/src/library/js/bootstrap.min.js', ['jquery'], false, true);
 }
 
 add_action('wp_enqueue_scripts', 'aquila_theme_scripts');
