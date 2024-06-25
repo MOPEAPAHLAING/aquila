6fd56b21372d6537b3bc80400c961fe3d511074b
MOEPAPAHLAING <moepapahlaing21@gmail.com>
2024-06-25 13:39:47 +0630

    add single post page

--- single.php
+++ single.php
@@ -1,12 +1,44 @@
 <?php
-/**
- * Page template
- * 
- * @package Aquila
- */
+/*
+*Sigle post template file
+*
+*@package aquila
+*/
+
 get_header();
 ?>
 
-<div>Single Post</div>
+	<div id="primary">
+		<main id="main" class="site-main mt-5" role="main">
+			<?php
+			if(have_posts()) :
+				?>
+				<div class="container">
+					
+					<?php
+					if(is_home() && !is_front_page()) {
+						?>
+						<header class="mb-5">
+							<h1 class="page-title screen-reader-text">
+								<?php single_post_title(); ?>
+							</h1>
+						</header>
+						<?php
+					}
+					while(have_posts()) : the_post();
+						get_template_part('template-parts/content');
+					endwhile;
+					?>
+				</div>
+				<?php
 
-<?php get_footer(); ?>
+				else :
+
+					get_template_part('template-parts/content-none');
+			endif;
+			?>
+		</main>
+	</div>
+	
+<?php
+get_footer();
