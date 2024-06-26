d545a5f299e21fcba2388b4daa258d65b65aa137
MOEPAPAHLAING <moepapahlaing21@gmail.com>
2024-06-26 13:17:09 +0630

    register widget sidebars

--- footer.php
+++ footer.php
@@ -5,8 +5,23 @@
  * @package Aquila
  */
 ?>
+<footer id="site-footer" class="bg-light p-4">
+	<div class="container color-gray">
+			<div class="row">
+				<section class="col-lg-4 col-md-6 col-sm-12">
+					<?php
+					if(is_active_sidebar('sidebar-2')) {
+						?>
+						<aside>
+							<?php dynamic_sidebar('sidebar-2');?>
+						</aside>
+						<?php
+					}
+					?>
+				</section>
+			</div>
 		</div>
-	</div>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
